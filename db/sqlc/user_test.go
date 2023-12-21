6a6b46db3b02744c3f41f6a36c0151b9f4965235
Ricardo Maricato <ricardo.maricato@hotmail.com>
2023-12-21 17:43:20 -0300

    feat: How to securely store passwords? Hash password in Go with Bcrypt

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -11,9 +11,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}

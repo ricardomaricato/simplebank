c8fe82e92d8067d76ad91b01a5b4b0fd0a419c01
Ricardo Maricato <ricardo.maricato@hotmail.com>
2024-02-15 17:32:54 -0300

    How to skip test in Go

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestSendEmailWithGmail(t *testing.T) {
-	//if testing.Short() {
-	//	t.Skip()
-	//}
+	if testing.Short() {
+		t.Skip()
+	}
 
 	config, err := util.LoadConfig("..")
 	require.NoError(t, err)
@@ -21,7 +21,7 @@ func TestSendEmailWithGmail(t *testing.T) {
 	subject := "A test email"
 	content := `
 	<h1>Hello world</h1>
-	<p>This is a test message from <a href="http://techschool.guru">Ricardo Maricato</a></p>
+	<p>This is a test message from <a href="https://www.linkedin.com/in/ricardomaricato/">Ricardo Maricato</a></p>
 	`
 	to := []string{"ricardo.maricato@hotmail.com"}
 	attachFiles := []string{"../README.md"}
